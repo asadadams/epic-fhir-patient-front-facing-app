cf43204009694fb3ab285e7ef2389626d7c1c9cc
Asad <clarkpeace.adams@gmail.com>
2026-03-13 13:48:29 +0000

    debugging

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,19 @@ function App() {
   const ISS = import.meta.env.VITE_ISS;
   const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
 
+  console.log('Environment variables:');
+  console.log(`EPIC_CLIENT_ID: ${EPIC_CLIENT_ID}`);
+  console.log(`ISS: ${ISS}`);
+  console.log(`REDIRECT_URI: ${REDIRECT_URI}`);
+
 
   const handleLogin = async () => {
     try {
       await FHIR.oauth2.authorize({
-        client_id: EPIC_CLIENT_ID,
-        scope: "launch openid profile patient/Observation.read patient/Condition.read patient/MedicationRequest.read offline_access",
-        iss: ISS || 'https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4',
-        redirect_uri: REDIRECT_URI || 'http://localhost:5173/callback/',
+        client_id: String(EPIC_CLIENT_ID),
+        scope: String(import.meta.env.VITE_SCOPE) || "launch openid profile patient/Observation.read patient/Condition.read patient/MedicationRequest.read offline_access",
+        iss: String(import.meta.env.VITE_ISS) || 'https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4',
+        redirect_uri: String(import.meta.env.VITE_REDIRECT_URI) || 'http://localhost:5173/callback/',
       });
     } catch (err) {
       console.error('Launch failed', err);
